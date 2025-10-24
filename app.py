ea9b440e99d5fc139ac8f0990edc597f59d48163
DowningAlex04 <downing.alex04@gmail.com>
2025-10-24 18:09:24 -0500

    draft(has errors)

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request  # NOT the same as requests 
-
+from github_api import get_github_user
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #home page
 def homepage():
     return render_template('index.html')
+
+@app.route('/get_user')
+def get_user_info():
+    #get info from github api, displays on page
+    print('form data is', request.args)
+    username = request.args.get('username') #returns none if no username
+#    username = request.args['username'] is more common, errors if there is no username
+    user_info, error_message = get_github_user(username) #returns a dict
+    if error_message:
+        return render_template('error.html', error=error_message)
+    else:
+        return render_template('github.html', user_info=user_info)
 
 
 if __name__ == '__main__':
--- github_api.py
+++ github_api.py
@@ -1,1 +1,30 @@
 # TODO make requests to github API
+import requests
+import logging
+def get_github_user(username):
+    #404 not found
+    #other errors
+    #success 
+
+    #return a tuple (data,error)
+    # if work (data, none), if not (none, error)
+    try:
+        response = requests.get(f'https://api.github.com/users/{username}')
+        if response.status_code == 404:
+            return None, f'Username {username} not found'
+        response.raise_for_status()
+        response_json = response.json()
+        user_info = extract_user_info(response_json)
+        return user_info, None
+    except Exception as e:
+        logging.exeption(e)
+        return None, 'Error'
+
+def extract_user_info(json_response):
+    return {
+        'login': json_response.get('login'),
+        'name': json_response.get('name'),
+        'avatar_url': json_response.get('avatar_url'),
+        'home_page': json_response.get('html_url'),
+        'repos': json_response.get('public_repos')
+    }
